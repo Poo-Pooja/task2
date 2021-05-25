e46cedb43b752701107b19bf59c5a442a1f9fe88
Poo-Pooja <76616245+Poo-Pooja@users.noreply.github.com>
2021-05-25 22:19:05 +0530

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,11 @@
+list1 = [12,-7,5,64,-14]
+num1 = list1
+for num1 in list1:
+  if num1 >= 0:
+    print(num1,end=" ")
+    
+list2 = [12,14,-95,3]
+num2 = list2
+for num2 in list2:
+  if num2 >= 0:
+    print(num2,end=" ")
